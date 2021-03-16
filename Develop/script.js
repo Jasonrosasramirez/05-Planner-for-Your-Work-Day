5ed5045f86a49e788186c08c1fad68060b174952
Jason Rosas Ramirez <jrosasrjf@gmail.com>
2021-03-15 22:59:41 -0500

    rearranged the button column and added a test button to play around with the classes

--- Develop/script.js
+++ Develop/script.js
@@ -4,7 +4,7 @@ var currentDayE1 = $("#currentDay");
 //displays the current date
 function displayTime() {
     var timeDateRightNow = moment().format("MMM DD, YYYY | hh:mm:ss a"); // moment gathers time information .format sets the display
-    currentDayE1.text(timeDateRightNow);
+    currentDayE1.text(timeDateRightNow); // the text of the currentDayE1 id tag will update with the actual time & date
 }
 
 setInterval(displayTime, 1000);
