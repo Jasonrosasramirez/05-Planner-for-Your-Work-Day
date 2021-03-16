ebff6267d8d1d4711c85673b604c0b95e2f01b9e
Jason Rosas Ramirez <jrosasrjf@gmail.com>
2021-03-16 01:09:04 -0500

    added the skelatons of the next time check and local storage functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,7 @@
 var tableBodyE1 = $(".tableBody");  //for traversing the table body to reference children
 var currentDayE1 = $("#currentDay");
 var timeBlockE1 = $(".time-block"); // The sections of each hour
+var hourE1 = $(".hour"); // The class displayed for the time of day 
 
 
 //displays the current date
@@ -11,23 +12,29 @@ function displayTime() {
 
 setInterval(displayTime, 1000);
 
+function timeCheck() {
+    //Am I late or am I early? 
+
+    var timeDateRightNow = moment().format("MMM DD, YYYY | hh:mm:ss a"); // moment gathers time information .format sets the display
+    
+    //still haven't mastered DOM traversal yet. Experimenting with just the 8am time block. Plan to make more modular later on  
+    var hour8amHeaderE1 = $("#hour8amHeader");
+
+    // if this happens in the future 
+    hour8amHeaderE1.children(".description").addClass("future"); // I now make the element green by adding the CSS class future 
+    
+    // if else this happens in the past 
+    // hour8amHeaderE1.children(".description").addClass("past"); // I now make the element red by adding the CSS class past
 
 
 
-//What i will be exp with 
-var hour8amHeaderE1 = $("#hour8amHeader");
-
-var hourE1 = $(".hour");
+}
 
 
-// var addingGreento8am = hourE1.first(); //so this turned the 8:00 am green
-//so the two lines below work 
-var row8E1 = $("#row8");
-//row8E1.addClass("future");
-
-hour8amHeaderE1.children(".description").addClass("future"); 
 
 
-var testQuery = document.getElementById("row8").getAttribute("class");
-console.log(testQuery);
-console.log("using the git method " + row8E1.get());
+
+
+
+
+
