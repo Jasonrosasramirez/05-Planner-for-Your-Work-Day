2da2c5d0a7c28547daa152f9fb638ed9832cfb77
Jason Rosas Ramirez <jrosasrjf@gmail.com>
2021-03-16 21:27:06 -0500

    experimenting with the inbetween method

--- Develop/script.js
+++ Develop/script.js
@@ -12,28 +12,56 @@ function displayTime() {
 
 setInterval(displayTime, 1000);
 
+
 function timeCheck() {
     //Am I late or am I early? 
 
-    var timeDateRightNow = moment().format("MMM DD, YYYY | hh:mm:ss a"); // moment gathers time information .format sets the display
-    
+    var timeCurrently_NoDate = moment().format("hh:mm:ss"); // moment gathers time information .format sets the display
+    console.log(timeCurrently_NoDate);
+
+
     //still haven't mastered DOM traversal yet. Experimenting with just the 8am time block. Plan to make more modular later on  
     var hour8amHeaderE1 = $("#hour8amHeader");
+
+// use moment() for these checks. Convert the time blocks to hh:mm:ss format
+
+    var format = "hh:mm:ss";
+    // var time = moment() gives you current time. no format required.
+    var time = moment('09:34:00', format);
+    var beforeTime = moment('07:00:00', format);
+    var afterTime = moment('09:00:00', format);
+
+    if (time.isBetween(beforeTime, afterTime)) {
+
+        console.log('is between')
+
+    } else {
+
+        console.log('is not between')
+
+    }
+
+
+
+
 
     // if this happens in the future 
     hour8amHeaderE1.children(".description").addClass("future"); // I now make the element green by adding the CSS class future 
     
-    // if else this happens in the past 
-    // hour8amHeaderE1.children(".description").addClass("past"); // I now make the element red by adding the CSS class past
+    // else if this happens in the past 
+    // hour8amHeaderE1.children(".description").addClass("past"); // I now make the element white by adding the CSS class past
 
-
-
+    // else 
+    //it's the present and it's red 
 }
 
+setInterval(timeCheck, 1000);
 
 
-
-
+function storeMeLocally () {
+    //triggered by the save buttons per row
+    // will probably involve the .data() method 
+}
 
 
 
